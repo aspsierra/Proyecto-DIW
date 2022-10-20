94a650004a37642ad2cffd1e5289e3d9882ca84b
Álvaro Sierra <asp.sierra@gmail.com>
2022-10-20 10:10:15 +0200

    animacion puntos

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,7 @@
 function showSlides(num) {
     var numSlider = num;
     var slides = document.getElementsByClassName("sliderElement");
+    var puntos = document.getElementsByClassName("punto");
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -8,8 +9,13 @@ function showSlides(num) {
     if (numSlider > slides.length) {
         numSlider = 1;
     }
+    for (let i = 0; i < puntos.length; i++) {
+        puntos[i].className = puntos[i].className.replace(" active", "")
+
+    }
     slides[numSlider - 1].style.display = "block";
-    setTimeout(showSlides, 2000, numSlider);
+    puntos[numSlider - 1].className += " active";
+    setTimeout(showSlides, 5000, numSlider);
 
 }
 
