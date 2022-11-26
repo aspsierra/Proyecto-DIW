4c54577e9e0fb33cea801a00c5372eb6d493d272
Álvaro Sierra <asp.sierra@gmail.com>
2022-11-26 12:04:16 +0100

    cambio visor

--- scripts/visor3D.js
+++ scripts/visor3D.js
@@ -1,9 +1,14 @@
 // Necessary for camera/plane rotation
 var degree = Math.PI / 180;
 
+//Contenedor
+/**
+ * @type {HTMLDivElement}
+ */
+ var visor = document.getElementById("Render")
 // Setup
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75,visor.scrollWidth/ 500, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 const controls = new THREE.OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
@@ -13,22 +18,18 @@ controls.enableDamping = true
 scene.background = new THREE.Color(0xF1EFED)
 //scene.add(new THREE.AxesHelper(250))
 
-/**
- * @type {HTMLDivElement}
- */
-var render = document.getElementById("Render")
+
 //var width = document.getElementById("Render").style.width;
 //var height = document.getElementById("Render").style.height
-renderer.setSize(700, 600);
+renderer.setSize(visor.scrollWidth, 500);
 
 document.getElementById("Render").appendChild(renderer.domElement);
 
 // Resize after viewport-size-change
 window.addEventListener("resize", function () {
-    var height = render.clientWidth
-    var width = document.getElementById("Render").style.width;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
+    visor.style.width=visor.scrollWidth+"px";
+    renderer.setSize(visor.scrollWidth, 500);
+    camera.aspect = visor.scrollWidth / 500;
     camera.updateProjectionMatrix();
 });
 
@@ -37,7 +38,7 @@ window.addEventListener("resize", function () {
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
 // Ground (comment out line: "scene.add( plane );" if Ground is not needed...)
-/*var plane = new THREE.Mesh(
+var plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(250, 250),
     new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
 );
@@ -45,14 +46,14 @@ scene.add(light);
 plane.position.x = 0
 plane.position.z = 0
 plane.rotation.x = -90 * degree;
-plane.position.y = 0;
-scene.add( plane );
+//plane.position.y = 0;
+//scene.add( plane );
 plane.receiveShadow = true;
 
-*/
+
 // ASCII file - STL Import
 var loader = new THREE.STLLoader();
-loader.load('../media/CalibrationCube_v2.stl', function (geometry) {
+loader.load('../media/dino.stl', function (geometry) {
     var material = new THREE.MeshNormalMaterial()
     //var material = new THREE.MeshLambertMaterial( { color: '0x049ef4', specular: 0x111111, shininess: 200 } );
     var mesh = new THREE.Mesh(geometry, material);
@@ -67,6 +68,9 @@ loader.load('../media/CalibrationCube_v2.stl', function (geometry) {
 
     mesh.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(
         -middle.x, -middle.y, -middle.z));
+
+    plane.position.y = -middle.z;
+    scene.add( plane );
 
     mesh.rotation.set(-Math.PI / 2, 0, 0)
     var largestDimension = Math.max(geometry.boundingBox.max.x,
@@ -92,9 +96,9 @@ loader.load('../media/CalibrationCube_v2.stl', function (geometry) {
 } );*/
 
 // Camera positioning
-camera.position.z = 400;
-camera.position.y = 300;
-camera.rotation.x = -45 * degree;
+camera.position.z = 200;
+camera.position.y = 200;
+camera.rotation.x = -90 * degree;
 
 // Ambient light (necessary for Phong/Lambert-materials, not for Basic)
 var ambientLight = new THREE.AmbientLight(0xffffff, 1);
