ff854cd9095cc938d8f9a40cc2bd5e9d406068ee
Álvaro Sierra <asp.sierra@gmail.com>
2022-12-28 11:08:15 +0100

    mostrar en web datos de JSON

--- scripts/loadJSON.js
+++ scripts/loadJSON.js
@@ -1,6 +1,21 @@
 async function load(){
-    const data = await fetch("../json/prueba.json").then(response => response.json());
-    console.log(data);
+    const DATA = await fetch("../json/prueba.json").then(response => response.json());
+    console.log(DATA);
+    return DATA;
 };
 
-load();
+function crearItem(item){
+    var div = document.createElement("div");
+    div.innerHTML="<h3>"+item.nombre+"</h3>"
+    document.getElementsByClassName("tabla")[0].appendChild(div);
+}
+
+async function generar(){
+    const DATOS = await load();
+    const MAT = DATOS.materiales;
+    for (let i = 0; i < MAT.length; i++) {
+        crearItem(MAT[i])
+    }
+}
+
+generar();
