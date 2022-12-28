3870d107234848aad8d0cc986991edd72a8c8d79
aspsierra <113519672+aspsierra@users.noreply.github.com>
2022-12-28 12:30:24 +0100

    Cambio carrusel (#4)
    * 14/11/22
    
    * reforma carrusel
    
    * margenes arreglados
    
    * cambios
    
    * borrados comentarios
    
    * 16/11/22
    
    * añadido visor 3d
    
    * cambio visor
    
    * carga de archivos

---
+++ scripts/formulario.js
@@ -0,0 +1,20 @@
+function selectColor() {
+    /**
+     * @type {HTMLSelectElement}
+     */
+    /*var selector = document.getElementById("color");
+    var opciones = document.getElementsByTagName("option");
+
+    selector.addEventListener("change", evt => {
+        selector.value=opcione
+        selector.innerHTML = selector.value
+    })*/
+
+
+}
+
+function main() {
+    selectColor();
+}
+
+main()
