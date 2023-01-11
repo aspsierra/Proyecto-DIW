eac3478aa9422e83454a059122b266548ef3b6c3
Álvaro Sierra <asp.sierra@gmail.com>
2023-01-11 12:39:26 +0100

    carga json tienda

--- scripts/loadJSON.js
+++ scripts/loadJSON.js
@@ -1,26 +1,55 @@
-async function load(){
-    const DATA = await fetch("../json/materials.json").then(response => response.json());
+async function load(nombreJson) {
+    const DATA = await fetch("../json/" + nombreJson + ".json").then(response => response.json());
     console.log(DATA);
     return DATA;
 };
 
-function crearItem(item){
+function crearItem(item) {
     var div = document.createElement("div");
     div.className = "item";
-    div.innerHTML = 
-        "<h3>" + item.nombre + "</h3>"+
-        "<h4>" + item.tipo + "</h4>" + 
-        "<img src = " + item.img + " alt = " + item.nombre + ">"+
+    div.innerHTML =
+        "<h3>" + item.nombre + "</h3>" +
+        "<h4>" + item.tipo + "</h4>" +
+        "<img src = " + item.img + " alt = " + item.nombre + ">" +
         "<p>" + item.descr + "</p>";
-    document.getElementsByClassName("tabla")[0].appendChild(div);
+    document.getElementsByClassName("tablaMaterial")[0].appendChild(div);
 }
 
-async function generar(){
-    const DATOS = await load();
-    const MAT = DATOS.materiales;
-    for (let i = 0; i < MAT.length; i++) {
-        crearItem(MAT[i])
+function crearItemTienda(item) {
+    var div = document.createElement("div");
+    div.className = "item";
+    div.innerHTML =
+
+        "<img src = " + item.img + " alt = " + item.nombre + ">" + "<p>" + item.nombre + "</p>" +
+        "<p>" + item.precio + "</p>";
+    document.getElementsByClassName("tablaTienda")[0].appendChild(div);
+}
+
+async function generar() {
+    var cargaJson;
+    var data;
+    var path = location.pathname.split("/")
+    switch (path[path.length - 1]) {
+        case "aboutMaterials.html":
+            cargaJson = await load("materials");
+            data = cargaJson.materiales;
+            for (let i = 0; i < data.length; i++) {
+                crearItem(data[i])
+            }
+            break;
+        case "shop.html":
+            cargaJson = await load("shop");
+            data = cargaJson.tienda;
+            for (let i = 0; i < data.length; i++) {
+                crearItemTienda(data[i])
+            }
+            break;
     }
+    //const DATOS = await load();
+    //const MAT = DATOS.materiales;
+    /*for (let i = 0; i < data.length; i++) {
+        crearItem(data[i])
+    }*/
 }
 
 generar();
